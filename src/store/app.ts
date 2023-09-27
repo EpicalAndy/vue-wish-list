0575a5e8f95a620057a0466f801e742af2f525eb
Andy <lun9nin@inbox.ru>
2023-09-27 19:39:36 +0300

    #7 add wishList and userList stores with actions

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,8 +1,20 @@
 // Utilities
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
+
+import { WishModel } from "@/models/wish-model";
+import { UserModel } from "@/models/user-model";
 
 export const useAppStore = defineStore('app', {
   state: () => ({
-    //
+    wishList: [] as Array<WishModel>,
+    userList: [] as Array<UserModel>
   }),
-})
+  actions: {
+    addWish(wish: WishModel) {
+      wish && this.wishList.push(wish);
+    },
+    addUser(user: UserModel) {
+      user && this.userList.push(user);
+    }
+  }
+});
